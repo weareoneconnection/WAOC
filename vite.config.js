db35886594a742bccfdcb55070228ce6ef975190
weareoneconnection <506473839open@gmail.com>
2025-12-26 18:36:10 +0800

    chore: update index.html with PWA icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icons/icon-192.png", "icons/icon-512.png", "manifest.webmanifest"]
+    })
+  ]
+});
